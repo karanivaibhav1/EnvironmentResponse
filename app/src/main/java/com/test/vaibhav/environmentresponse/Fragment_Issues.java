1b575e1523011326a587aa485ced469c9e38fad2
karanivaibhav1 <karanivaibhav1@gmail.com>
2016-04-21 16:15:35 -0400

    04/20/2016

--- app/src/main/java/com/test/vaibhav/environmentresponse/Fragment_Issues.java
+++ app/src/main/java/com/test/vaibhav/environmentresponse/Fragment_Issues.java
@@ -119,9 +119,7 @@ public class Fragment_Issues extends Fragment {
             public void onClick(View v){
                 Intent galleryIntent = new Intent(Intent.ACTION_PICK, MediaStore.Images.Media.EXTERNAL_CONTENT_URI);
                 startActivityForResult(galleryIntent, RESULT_LOAD_IMAGE);
-
             }
-
         });
 
         return rootView;
